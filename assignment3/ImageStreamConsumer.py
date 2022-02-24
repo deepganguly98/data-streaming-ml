db4ea30fd9471aeb223a9c44d357f87c5a413b2c
deepganguly98 <diganto.ganguly@gmail.com>
2022-02-24 23:48:13 +0530

    fucntions added

--- assignment3/ImageStreamConsumer.py
+++ assignment3/ImageStreamConsumer.py
@@ -1,38 +1,35 @@
-from json import loads  
 from kafka import KafkaConsumer
 import base64
 from io import BytesIO
 from PIL import Image
-from keras.preprocessing.image import load_img
 from keras.preprocessing.image import img_to_array
 from keras.models import load_model
 import numpy as np
 
-class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat','Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
-# load model
-model = load_model('/Users/deep98/Desktop/vector-test/assignment1/mnist_fashion_predict.h5')
-
-# decodeit = open('new_file.png', 'wb')
-i=0
-my_consumer = KafkaConsumer('test-topic',bootstrap_servers ='localhost : 9092',auto_offset_reset = 'latest')
-for message in my_consumer:  
-    message = message.value
-    # print(message)
-    # decodeit.write()
-    # decodeit.write(message[1:-1])
-    im_d64 = message[1:-1]
-    # decodeit.write(base64.b64decode(im_d64))
-    # print(im_d64)
-    im_file = BytesIO(base64.b64decode(im_d64))  # convert image to file-like object
+def prep_image(msg):
+    im_file = BytesIO(base64.b64decode(msg))  # convert image to file-like object
     img = Image.open(im_file).convert('L')
     img = img_to_array(img)
     img = img.reshape(1, 28, 28, 1)
     # prepare pixel data
     img = img.astype('float32')
     img = img / 255.0
+    return img
+
+def predict_label(img):
+    class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat','Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
     pred = model.predict(img)
     x = np.argmax(pred)
-    print(i,"-",type(img)," Predicted Label = ",class_names[x])
+    return class_names[x]
+
+# load model
+model = load_model('mnist_fashion_predict.h5')
+i=0
+my_consumer = KafkaConsumer('predict',bootstrap_servers ='localhost : 9092',auto_offset_reset = 'latest')
+for message in my_consumer:  
+    message = message.value
+    im_d64 = message[1:-1]
+    img = prep_image(im_d64)
+    label = predict_label(img)
+    print(i,"-",type(img)," Predicted Label = ",label)
     i=i+1
-    
-# decodeit.close()
--- assignment3/ImageStreamProducer.py
+++ assignment3/ImageStreamProducer.py
@@ -9,16 +9,19 @@ from kafka import KafkaProducer
 
 (trainX, trainY) , (testX, testY) = fashion_mnist.load_data()
 
-my_producer = KafkaProducer(bootstrap_servers = ['localhost:9092'],value_serializer = lambda x:dumps(x).encode('utf-8'))
-
-for i in range(0,testX.shape[0]):
-    im = Image.fromarray(np.uint8(trainX[i])).convert('RGB')
-    print(i," ",type(im))
-    # im.show()
+def img_conv(imgarr):
+    im = Image.fromarray(np.uint8(imgarr)).convert('RGB')
     im_file = BytesIO()
     im.save(im_file, format="PNG")
     im_bytes = im_file.getvalue()  # im_bytes: image in binary format.
     im_b64 = base64.b64encode(im_bytes).decode('utf-8')
+    return im_b64
+
+
+my_producer = KafkaProducer(bootstrap_servers = ['localhost:9092'],value_serializer = lambda x:dumps(x).encode('utf-8'))
+
+for i in range(0,250):
+    im_b64 = img_conv(trainX[i])
     print(im_b64)
-    my_producer.send('test-topic', value = im_b64)
+    my_producer.send('predict', value = im_b64)
     my_producer.flush()
